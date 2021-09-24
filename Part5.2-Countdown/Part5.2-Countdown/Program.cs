b0f3db6358624eb47943fd2cac21a5fa03ee979f
bucketfro <89718597+bucketfro@users.noreply.github.com>
2021-09-24 18:13:32 -0500

    Add files via upload

---
+++ Part5.2-Countdown/Part5.2-Countdown/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Part5._2_Countdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = "--- Countdown ---";
+            Console.SetCursorPosition((Console.WindowWidth - title.Length) / 2, Console.CursorTop);
+            Console.WriteLine(title);
+
+            bool isNumber = false;
+            int countby = 0;
+
+            while (!isNumber)
+            {
+                Console.WriteLine("What number would you like to count down by?");
+                isNumber = Int32.TryParse(Console.ReadLine(), out countby);
+            }
+
+            isNumber = false;
+            int start = 0;
+
+            while (!isNumber)
+            {
+                Console.WriteLine("What number would you like to start with?");
+                isNumber = Int32.TryParse(Console.ReadLine(), out start);
+            }
+
+            while (countby <= start)
+            {
+                if (start % countby == 0)
+                {
+                    Console.WriteLine(start);
+                }
+                start--;
+            }
+
+        }
+    }
+}
